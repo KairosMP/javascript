66aa06abb4968a897f0559334e44cbf5a07142c2
KairosMP <109192850+KairosMP@users.noreply.github.com>
2022-08-18 16:25:49 -0400

    ex008 a ex0010
    adicionados exercícios.

--- exercicios/ex008/ex008.js
+++ exercicios/ex008/ex008.js
@@ -1,3 +1,6 @@
-var vel =  60.5
+var vel =  60
 console.log(`A velocidade do seu carro é ${vel} Km/h.`)
-console.log('dirija sempre com sinto de segurantaça')
+if (vel > 60) { // condição simples:
+    console.log('Você ultrapassou a velocidade. Multado!')
+}
+console.log('diriga sempre usando cinto de segurança.')
