9ac4a65e0cfcddf682cc902af036c13f4fd7a319
Miller <109192850+KairosMP@users.noreply.github.com>
2022-08-30 23:00:21 -0400

    conclusão do ex015

--- exercicios/ex014/script.js
+++ exercicios/ex014/script.js
@@ -1,1 +1,21 @@
-alert('Olá')
+function carregar(params) {
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+var hora = 15
+msg.innerHTML = `Agora são ${hora} horas.`
+if (hora >= 0 && hora < 12) {
+    // Bom dia!
+    img.src = 'imagens/manha.png'
+    document.body.style.background = '#FFD782'
+} else if (hora >= 12 && hora < 18) {
+    // Boa tarde!
+    img.src = 'imagens/oceano.png'
+    document.body.style.background = '#EC781B'
+} else {
+    // Boa noite!
+    img.src = 'imagens/noite.png'
+    document.body.style.background = '#415870'
+}
+}
