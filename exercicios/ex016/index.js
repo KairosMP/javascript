d9bf45effc1a3ba3782ec64ade10a7ccd94c70c0
Miller <109192850+KairosMP@users.noreply.github.com>
2022-09-02 03:46:08 -0400

    update ex

---
+++ exercicios/ex016/index.js
@@ -0,0 +1,17 @@
+for (let c = 1; c <= 6; c++) {
+    console.log(`Passo ${c}`)
+}
+
+/*
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+*/
+
+/*
+while(c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+*/
