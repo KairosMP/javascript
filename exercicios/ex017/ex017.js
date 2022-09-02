8e328961bf199af380ad56cc6b7eba9ee59f8551
Miller <109192850+KairosMP@users.noreply.github.com>
2022-09-02 12:35:53 -0400

    ex017

--- exercicios/ex017/ex017.js
+++ exercicios/ex017/ex017.js
@@ -1,13 +1,6 @@
-/*
-var c = 1
-while (c <= 5) {
-    console.log(c)
-    c++
-}
-*/
-
-//------------------------------//
+console.log('vai começar....')
 
 for (let c = 1; c <= 5; c++) {
     console.log(c)
 }
+ console.log('Fim!')
